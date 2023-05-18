9c99ef3a7d58199739fc6a8502bf4cb8225f2276
antlerjely96 <antlerjely96@gmail.com>
2023-05-18 10:37:51 +0700

    add demo class

---
+++ ConsoleApp/demo1/demo1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nhập số a. Kiểm tra a là âm hay dương
+
+//Khai báo a
+int a;
+//Hiển thị dòng chữ a =
+Console.Write("a = ");
+//Nhập a
+a = int.Parse(Console.ReadLine());
+//Kiểm tra a
+if (a > 0)
+{
+    Console.WriteLine(a + " la so duong");
+}
+else if (a < 0)
+{
+    Console.WriteLine(a + " la so am");
+}
+else
+{
+    Console.WriteLine(a + " la so khong duong khong am");
+}
---
+++ ConsoleApp/demo2/demo2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Nhập mảng có n phần tử là số nguyên. Tính tổng các phần tử
+
+//Khai báo n
+int n;
+//Nhập n
+Console.Write("n = ");
+n = int.Parse(Console.ReadLine());
+//Khai báo mảng số nguyên có n phần tử
+int[] arr = new int[n];
+//Nhập mảng
+for (int i = 0; i < n; i++)
+{
+    Console.Write("arr[" + i + "] = ");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+//Khai báo biến tong
+int tong = 0;
+//Tính tổng các phần tử
+for (int i = 0; i < n; i++)
+{
+    tong += arr[i];
+}
+//Hiển thị tổng
+Console.WriteLine("Tổng các phần tử của mảng: " + tong);
