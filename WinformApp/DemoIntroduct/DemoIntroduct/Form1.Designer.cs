002a9e3b3f410387bfdef167c3dd5c1b398fccd2
antlerjely96 <antlerjely96@gmail.com>
2023-05-20 11:11:53 +0700

    add demo winform P1

---
+++ WinformApp/DemoIntroduct/DemoIntroduct/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace DemoIntroduct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_a = new System.Windows.Forms.TextBox();
+            this.btn_display = new System.Windows.Forms.Button();
+            this.tb_b = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_tong = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(182, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a:";
+            // 
+            // tb_a
+            // 
+            this.tb_a.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_a.Location = new System.Drawing.Point(259, 58);
+            this.tb_a.Name = "tb_a";
+            this.tb_a.Size = new System.Drawing.Size(286, 38);
+            this.tb_a.TabIndex = 1;
+            // 
+            // btn_display
+            // 
+            this.btn_display.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_display.Location = new System.Drawing.Point(239, 300);
+            this.btn_display.Name = "btn_display";
+            this.btn_display.Size = new System.Drawing.Size(149, 47);
+            this.btn_display.TabIndex = 2;
+            this.btn_display.Text = "Tính";
+            this.btn_display.UseVisualStyleBackColor = true;
+            this.btn_display.Click += new System.EventHandler(this.btn_display_Click);
+            // 
+            // tb_b
+            // 
+            this.tb_b.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_b.Location = new System.Drawing.Point(259, 133);
+            this.tb_b.Name = "tb_b";
+            this.tb_b.Size = new System.Drawing.Size(286, 38);
+            this.tb_b.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(182, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "b:";
+            // 
+            // tb_tong
+            // 
+            this.tb_tong.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_tong.Enabled = false;
+            this.tb_tong.Location = new System.Drawing.Point(259, 206);
+            this.tb_tong.Name = "tb_tong";
+            this.tb_tong.Size = new System.Drawing.Size(286, 38);
+            this.tb_tong.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(119, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 37);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "a + b =";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tb_tong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tb_b);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_display);
+            this.Controls.Add(this.tb_a);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TextBox tb_tong;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.TextBox tb_b;
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button btn_display;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_a;
+
+        #endregion
+    }
+}
