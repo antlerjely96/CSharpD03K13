78c3ba098bb5726718e6cb79d02f971de4bf6a6e
antlerjely96 <antlerjely96@gmail.com>
2023-05-23 10:47:43 +0700

    add chua bai tap 1

---
+++ WinformApp/ChuaBaiTap1/ChuaBaiTap1/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ChuaBaiTap1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.tbTong = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbHieu = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTich = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbThuong = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnTong = new System.Windows.Forms.Button();
+            this.btnHieu = new System.Windows.Forms.Button();
+            this.btnTich = new System.Windows.Forms.Button();
+            this.btnThuong = new System.Windows.Forms.Button();
+            this.btnTinhAll = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(81, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a =";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(81, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "b =";
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(190, 60);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(304, 38);
+            this.tbA.TabIndex = 2;
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(190, 145);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(304, 38);
+            this.tbB.TabIndex = 3;
+            // 
+            // tbTong
+            // 
+            this.tbTong.Enabled = false;
+            this.tbTong.Location = new System.Drawing.Point(190, 238);
+            this.tbTong.Name = "tbTong";
+            this.tbTong.Size = new System.Drawing.Size(304, 38);
+            this.tbTong.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(81, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 55);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "a + b =";
+            // 
+            // tbHieu
+            // 
+            this.tbHieu.Enabled = false;
+            this.tbHieu.Location = new System.Drawing.Point(190, 338);
+            this.tbHieu.Name = "tbHieu";
+            this.tbHieu.Size = new System.Drawing.Size(304, 38);
+            this.tbHieu.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(81, 341);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 55);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "a - b =";
+            // 
+            // tbTich
+            // 
+            this.tbTich.Enabled = false;
+            this.tbTich.Location = new System.Drawing.Point(190, 434);
+            this.tbTich.Name = "tbTich";
+            this.tbTich.Size = new System.Drawing.Size(304, 38);
+            this.tbTich.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(81, 437);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 55);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "a * b =";
+            // 
+            // tbThuong
+            // 
+            this.tbThuong.Enabled = false;
+            this.tbThuong.Location = new System.Drawing.Point(190, 533);
+            this.tbThuong.Name = "tbThuong";
+            this.tbThuong.Size = new System.Drawing.Size(304, 38);
+            this.tbThuong.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(81, 536);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 55);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "a / b =";
+            // 
+            // btnTong
+            // 
+            this.btnTong.Location = new System.Drawing.Point(643, 51);
+            this.btnTong.Name = "btnTong";
+            this.btnTong.Size = new System.Drawing.Size(199, 47);
+            this.btnTong.TabIndex = 12;
+            this.btnTong.Text = "a + b";
+            this.btnTong.UseVisualStyleBackColor = true;
+            this.btnTong.Click += new System.EventHandler(this.btnTong_Click);
+            // 
+            // btnHieu
+            // 
+            this.btnHieu.Location = new System.Drawing.Point(643, 145);
+            this.btnHieu.Name = "btnHieu";
+            this.btnHieu.Size = new System.Drawing.Size(199, 47);
+            this.btnHieu.TabIndex = 13;
+            this.btnHieu.Text = "a - b";
+            this.btnHieu.UseVisualStyleBackColor = true;
+            this.btnHieu.Click += new System.EventHandler(this.btnHieu_Click);
+            // 
+            // btnTich
+            // 
+            this.btnTich.Location = new System.Drawing.Point(643, 238);
+            this.btnTich.Name = "btnTich";
+            this.btnTich.Size = new System.Drawing.Size(199, 47);
+            this.btnTich.TabIndex = 14;
+            this.btnTich.Text = "a * b";
+            this.btnTich.UseVisualStyleBackColor = true;
+            this.btnTich.Click += new System.EventHandler(this.btnTich_Click);
+            // 
+            // btnThuong
+            // 
+            this.btnThuong.Location = new System.Drawing.Point(643, 333);
+            this.btnThuong.Name = "btnThuong";
+            this.btnThuong.Size = new System.Drawing.Size(199, 47);
+            this.btnThuong.TabIndex = 15;
+            this.btnThuong.Text = "a / b";
+            this.btnThuong.UseVisualStyleBackColor = true;
+            this.btnThuong.Click += new System.EventHandler(this.btnThuong_Click);
+            // 
+            // btnTinhAll
+            // 
+            this.btnTinhAll.Location = new System.Drawing.Point(643, 429);
+            this.btnTinhAll.Name = "btnTinhAll";
+            this.btnTinhAll.Size = new System.Drawing.Size(199, 47);
+            this.btnTinhAll.TabIndex = 16;
+            this.btnTinhAll.Text = "a + - * / b";
+            this.btnTinhAll.UseVisualStyleBackColor = true;
+            this.btnTinhAll.Click += new System.EventHandler(this.btnTinhAll_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(643, 524);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(194, 54);
+            this.btnReset.TabIndex = 17;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1369, 870);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnTinhAll);
+            this.Controls.Add(this.btnThuong);
+            this.Controls.Add(this.btnTich);
+            this.Controls.Add(this.btnHieu);
+            this.Controls.Add(this.btnTong);
+            this.Controls.Add(this.tbThuong);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbTich);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbHieu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbTong);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnReset;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.TextBox tbTong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbHieu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTich;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbThuong;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnTong;
+        private System.Windows.Forms.Button btnHieu;
+        private System.Windows.Forms.Button btnTich;
+        private System.Windows.Forms.Button btnThuong;
+        private System.Windows.Forms.Button btnTinhAll;
+
+        #endregion
+    }
+}
---
+++ WinformApp/ChuaBaiTap1/ChuaBaiTap1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChuaBaiTap1
+{
+    public partial class Form1 : Form
+    {
+        
+        //Khai báo a, b
+        private double a, b;
+
+        private double nhap(TextBox textBox)
+        {
+            double n = double.Parse(textBox.Text);
+            return n;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTong_Click(object sender, EventArgs e)
+        {
+            //Lấy a
+            a = nhap(tbA);
+            //Lấy b
+            b = nhap(tbB);
+            //Tính a + b
+            double tong = a + b;
+            //Hiển thị kết quả
+            tbTong.Text = tong.ToString();
+        }
+
+        private void btnHieu_Click(object sender, EventArgs e)
+        {
+            //Lấy a
+            a = nhap(tbA);
+            //Lấy b
+            b = nhap(tbB);
+            //Tính a - b
+            double hieu = a - b;
+            //Hiển thị kết quả
+            tbHieu.Text = hieu.ToString();
+        }
+
+        private void btnTich_Click(object sender, EventArgs e)
+        {
+            //Lấy a
+            a = nhap(tbA);
+            //Lấy b
+            b = nhap(tbB);
+            //Tính a * b
+            double tich = a * b;
+            //Hiển thị kết quả
+            tbTich.Text = tich.ToString();
+        }
+
+        private void btnThuong_Click(object sender, EventArgs e)
+        {
+            //Lấy a
+            a = nhap(tbA);
+            //Lấy b
+            b = nhap(tbB);
+            //Tính a / b
+            double thuong = a / b;
+            //Hiển thị kết quả
+            tbThuong.Text = thuong.ToString();
+        }
+
+        private void btnTinhAll_Click(object sender, EventArgs e)
+        {
+            btnTong_Click(null, null);
+            btnHieu_Click(null, null);
+            btnTich_Click(null, null);
+            btnThuong_Click(null, null);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tbA.Text = tbB.Text = tbTong.Text = tbHieu.Text = tbTich.Text = tbThuong.Text = "";
+        }
+    }
+}
