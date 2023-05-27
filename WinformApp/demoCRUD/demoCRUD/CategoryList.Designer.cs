612d1e8388891555a785ce8c1a132e75dc038378
antlerjely96 <antlerjely96@gmail.com>
2023-05-27 11:39:51 +0700

    add demo update, delete

---
+++ WinformApp/demoCRUD/demoCRUD/CategoryList.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System.ComponentModel;
+
+namespace demoCRUD
+{
+    partial class CategoryList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCategoryList = new System.Windows.Forms.DataGridView();
+            this.lbNameCat = new System.Windows.Forms.Label();
+            this.tbCatName = new System.Windows.Forms.TextBox();
+            this.btnAddNewCat = new System.Windows.Forms.Button();
+            this.btnSaveCat = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.tbCatId = new System.Windows.Forms.TextBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoryList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCategoryList
+            // 
+            this.dgvCategoryList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvCategoryList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvCategoryList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvCategoryList.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvCategoryList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCategoryList.GridColor = System.Drawing.SystemColors.MenuHighlight;
+            this.dgvCategoryList.Location = new System.Drawing.Point(32, 27);
+            this.dgvCategoryList.Name = "dgvCategoryList";
+            this.dgvCategoryList.ReadOnly = true;
+            this.dgvCategoryList.RowTemplate.Height = 40;
+            this.dgvCategoryList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvCategoryList.Size = new System.Drawing.Size(1053, 318);
+            this.dgvCategoryList.TabIndex = 0;
+            this.dgvCategoryList.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCategoryList_CellClick);
+            // 
+            // lbNameCat
+            // 
+            this.lbNameCat.Location = new System.Drawing.Point(41, 396);
+            this.lbNameCat.Name = "lbNameCat";
+            this.lbNameCat.Size = new System.Drawing.Size(240, 42);
+            this.lbNameCat.TabIndex = 1;
+            this.lbNameCat.Text = "Category Name";
+            // 
+            // tbCatName
+            // 
+            this.tbCatName.Enabled = false;
+            this.tbCatName.Location = new System.Drawing.Point(287, 393);
+            this.tbCatName.Name = "tbCatName";
+            this.tbCatName.Size = new System.Drawing.Size(243, 38);
+            this.tbCatName.TabIndex = 2;
+            // 
+            // btnAddNewCat
+            // 
+            this.btnAddNewCat.Location = new System.Drawing.Point(57, 492);
+            this.btnAddNewCat.Name = "btnAddNewCat";
+            this.btnAddNewCat.Size = new System.Drawing.Size(152, 53);
+            this.btnAddNewCat.TabIndex = 3;
+            this.btnAddNewCat.Text = "Add new";
+            this.btnAddNewCat.UseVisualStyleBackColor = true;
+            this.btnAddNewCat.Click += new System.EventHandler(this.btnAddNewCat_Click);
+            // 
+            // btnSaveCat
+            // 
+            this.btnSaveCat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSaveCat.Enabled = false;
+            this.btnSaveCat.Location = new System.Drawing.Point(256, 492);
+            this.btnSaveCat.Name = "btnSaveCat";
+            this.btnSaveCat.Size = new System.Drawing.Size(152, 53);
+            this.btnSaveCat.TabIndex = 4;
+            this.btnSaveCat.Text = "Save";
+            this.btnSaveCat.UseVisualStyleBackColor = true;
+            this.btnSaveCat.Click += new System.EventHandler(this.btnSaveCat_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnUpdate.Enabled = false;
+            this.btnUpdate.Location = new System.Drawing.Point(460, 492);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(152, 53);
+            this.btnUpdate.TabIndex = 5;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // tbCatId
+            // 
+            this.tbCatId.Location = new System.Drawing.Point(556, 393);
+            this.tbCatId.Name = "tbCatId";
+            this.tbCatId.Size = new System.Drawing.Size(184, 38);
+            this.tbCatId.TabIndex = 6;
+            this.tbCatId.Visible = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnDelete.Enabled = false;
+            this.btnDelete.Location = new System.Drawing.Point(662, 492);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(152, 53);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnReset.Location = new System.Drawing.Point(876, 492);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(152, 53);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // CategoryList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 747);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.tbCatId);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSaveCat);
+            this.Controls.Add(this.btnAddNewCat);
+            this.Controls.Add(this.tbCatName);
+            this.Controls.Add(this.lbNameCat);
+            this.Controls.Add(this.dgvCategoryList);
+            this.Name = "CategoryList";
+            this.Text = "CategoryList";
+            this.Load += new System.EventHandler(this.CategoryList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCategoryList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnReset;
+
+        private System.Windows.Forms.Button btnDelete;
+
+        private System.Windows.Forms.TextBox tbCatId;
+
+        private System.Windows.Forms.Button btnUpdate;
+
+        private System.Windows.Forms.Label lbNameCat;
+        private System.Windows.Forms.TextBox tbCatName;
+        private System.Windows.Forms.Button btnAddNewCat;
+        private System.Windows.Forms.Button btnSaveCat;
+
+        private System.Windows.Forms.DataGridView dgvCategoryList;
+
+        #endregion
+    }
+}
---
+++ WinformApp/demoCRUD/demoCRUD/CategoryList.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace demoCRUD
+{
+    public partial class CategoryList : Form
+    {
+        //chuỗi để config kết nối
+        private static string ConnectionString =
+            "server = localhost; user id = root; password = ; database = d03k13csharp";
+        //Tạo kết nối
+        private MySqlConnection Connection = new MySqlConnection(ConnectionString);
+        //Biến sql
+        private string sql;
+        public CategoryList()
+        {
+            InitializeComponent();
+        }
+
+        private void CategoryList_Load(object sender, EventArgs e)
+        {
+            //Query
+            sql = "SELECT * FROM categories";
+            //Tạo đối tượng MySqlDataAdapter để chạy query và lấy dữ liệu
+            MySqlDataAdapter adapter = new MySqlDataAdapter(sql, Connection);
+            //Tạo đối tượng DataTable để chứa dữ liệu lấy ược từ adapter
+            DataTable dataTable = new DataTable();
+            //Lưu dữ liệu từ adapter vào dataTable
+            adapter.Fill(dataTable);
+            //Lưu dataTable vào DataGridView
+            dgvCategoryList.DataSource = dataTable;
+        }
+
+        private void btnAddNewCat_Click(object sender, EventArgs e)
+        {
+            //Cho phép tương tác với nút Save và ô input nhập tên
+            btnSaveCat.Enabled = true;
+            tbCatName.Enabled = true;
+            //Không cho tương tác với nút Add new
+            btnAddNewCat.Enabled = false;
+        }
+
+        private void btnSaveCat_Click(object sender, EventArgs e)
+        {
+            //Lấy dữ liệu ở ô input nhập tên
+            string CatName = tbCatName.Text;
+            //Viết query
+            sql = "INSERT INTO categories(name) VALUES ('" + CatName + "')";
+            //Tạo command bằng đối tượng MySqlCommand để chạy query
+            MySqlCommand command = new MySqlCommand(sql, Connection);
+            //Mở kết nối
+            Connection.Open();
+            //Chạy query
+            command.ExecuteNonQuery();
+            //Đóng kết nối
+            Connection.Close();
+            //Hiển thị thông báo
+            MessageBox.Show("Thêm thành công");
+            //Load lại DâataGridView
+            CategoryList_Load(null, null);
+            //Không cho tương tác với nút Save và ô input nhập tên
+            btnSaveCat.Enabled = false;
+            tbCatName.Enabled = false;
+            //Cho tương tác với nút Add new
+            btnAddNewCat.Enabled = true;
+        }
+
+        private void dgvCategoryList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Lấy dữ liệu ở hàng đang được chọn
+            string CatId = dgvCategoryList.Rows[e.RowIndex].Cells["id"].Value.ToString();
+            string CatName = dgvCategoryList.Rows[e.RowIndex].Cells["name"].Value.ToString();
+            
+            //enable các ô input và btnUpdate
+            tbCatName.Enabled = true;
+            btnUpdate.Enabled = true;
+            btnDelete.Enabled = true;
+            btnAddNewCat.Enabled = false;
+            
+            //Đổ dữ liệu vào input
+            tbCatId.Text = CatId;
+            tbCatName.Text = CatName;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //Lấy dữ liệu trong ô input
+            string CatId = tbCatId.Text;
+            string CatName = tbCatName.Text;
+            //Query
+            sql = "UPDATE categories SET name = '" + CatName + "' WHERE id = '" + CatId + "'";
+            //Tạo command
+            MySqlCommand command = new MySqlCommand(sql, Connection);
+            //Mở kết nối
+            Connection.Open();
+            //chạy query
+            command.ExecuteNonQuery();
+            //Đóng kết nối
+            Connection.Close();
+            //Hiển thị thông báo
+            MessageBox.Show("Sửa thành công");
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            tbCatName.Enabled = false;
+            btnAddNewCat.Enabled = true;
+            //Load lại danh sách
+            CategoryList_Load(null, null);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            //Lấy id bản ghi cần xóa
+            string CatId = tbCatId.Text;
+            //Query
+            sql = "DELETE FROM categories WHERE id = '" + CatId + "'";
+            //Tạo command
+            MySqlCommand command = new MySqlCommand(sql, Connection);
+            //Mở kết nối
+            Connection.Open();
+            //Chạy query
+            command.ExecuteNonQuery();
+            //Đóng kết nối
+            Connection.Close();
+            //Hiển thị thông báo
+            MessageBox.Show("Xóa thành công");
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            tbCatName.Enabled = false;
+            btnAddNewCat.Enabled = true;
+            //Load lại danh sách
+            CategoryList_Load(null, null);
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            tbCatName.Enabled = false;
+            btnSaveCat.Enabled = false;
+            btnUpdate.Enabled = false;
+            btnDelete.Enabled = false;
+            btnAddNewCat.Enabled = true;
+
+            tbCatId.Text = "";
+            tbCatName.Text = "";
+        }
+    }
+}
